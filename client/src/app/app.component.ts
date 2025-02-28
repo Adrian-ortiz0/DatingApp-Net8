d7fbd8c7fc63d3f2521dbc93a2d6c85f50a256ba
Adrian-ortiz0 <dxniel7328@gmail.com>
2025-02-28 12:54:59 -0500

    feat: :sparkles: I've finished the login and register section using signals

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,23 +1,19 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit, inject } from '@angular/core';
 import { NavComponent } from "./nav/nav.component";
 import { AccountService } from './_services/account.service';
+import { HomeComponent } from "./home/home.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: true,
   styleUrls: ['./app.component.css'],
-  imports: [NavComponent] 
+  imports: [NavComponent, HomeComponent] 
 })
 export class AppComponent implements OnInit {
-  private http = inject(HttpClient);
   private accountService = inject(AccountService);
-  title = 'DatingApp';
-  users: any;
 
   ngOnInit(): void {
-    this.getUsers();
     this.setCurrentUser();
   }
   setCurrentUser(){
@@ -29,11 +25,4 @@ export class AppComponent implements OnInit {
     this.accountService.currentUser.set(user);
   }
 
-  getUsers(){
-    this.http.get("https://localhost:5001/api/users").subscribe({
-      next: response => this.users = response,
-      error: error => console.error("Error fetching users", error),
-      complete: () => console.log("Request has completed!")
-    });
-  }
 }
