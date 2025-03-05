96aa8c0123ff645580ca7277e14f31ecefc5320d
Adrian-ortiz0 <dxniel7328@gmail.com>
2025-03-05 09:32:54 -0500

    feat ✨: I've added the updateMemberDto to create that functonality on the client

--- DatingApp/Controllers/UsersController.cs
+++ DatingApp/Controllers/UsersController.cs
@@ -1,4 +1,5 @@
-﻿using AutoMapper;
+﻿using System.Security.Claims;
+using AutoMapper;
 using DatingApp.Data;
 using DatingApp.DTOs;
 using DatingApp.Entities;
@@ -10,7 +11,7 @@ using Microsoft.EntityFrameworkCore;
 namespace DatingApp.Controllers
 {
     [Authorize]
-    public class UsersController(IUserRepository userRepository) : BaseApiController
+    public class UsersController(IUserRepository userRepository, IMapper mapper) : BaseApiController
     {
 
         [HttpGet]
@@ -31,6 +32,24 @@ namespace DatingApp.Controllers
             }
 
             return user;
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+        {
+            var name = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            
+            if(name == null) return BadRequest("No username found in token");
+
+            var user = await userRepository.GetUserByNameAsync(name);
+
+            if (user == null) return BadRequest("Could not find user");
+            
+            mapper.Map(memberUpdateDto, user);
+            
+            if(await userRepository.SaveAllAsync()) return NoContent();
+            
+            return BadRequest("Failed to update user");
         }
 
     }
--- client/src/app/members/member-edit/member-edit.component.ts
+++ client/src/app/members/member-edit/member-edit.component.ts
@@ -1,4 +1,10 @@
-import { Component, inject, OnInit, ViewChild } from '@angular/core';
+import {
+  Component,
+  HostListener,
+  inject,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { Member } from '../../_models/member';
 import { AccountService } from '../../_services/account.service';
 import { MembersService } from '../../_services/members.service';
@@ -11,31 +17,38 @@ import { ToastrService } from 'ngx-toastr';
   standalone: true,
   imports: [TabsModule, FormsModule],
   templateUrl: './member-edit.component.html',
-  styleUrl: './member-edit.component.css'
+  styleUrl: './member-edit.component.css',
 })
 export class MemberEditComponent implements OnInit {
-  @ViewChild("editForm") editForm?: NgForm;
+  @ViewChild('editForm') editForm?: NgForm;
+  @HostListener('window:beforeunload', ['$event']) notify($event: any) {
+    if (this.editForm?.dirty) {
+      $event.returnValue = true;
+    }
+  }
   member?: Member;
   private accountService = inject(AccountService);
   private memberService = inject(MembersService);
   private toastr = inject(ToastrService);
 
   ngOnInit(): void {
-      this.loadMember();
+    this.loadMember();
   }
 
-  loadMember(){
+  loadMember() {
     const user = this.accountService.currentUser();
-    if(!user) return;
+    if (!user) return;
     this.memberService.getMember(user.username).subscribe({
-      next: member => this.member = member
-    })
+      next: (member) => (this.member = member),
+    });
   }
 
-  updateMember(){
-    console.log(this.member);
-    this.toastr.success("Profile updated successfully");
-    this.editForm?.reset(this.member);
+  updateMember() {
+    this.memberService.updateMember(this.editForm?.value).subscribe({
+      next: (_) => {
+        this.toastr.success('Profile updated successfully');
+        this.editForm?.reset(this.member);
+      },
+    });
   }
-
 }
