5c0e4afc5cf278f52b72af52002da5c79a979f30
Adrian-ortiz0 <dxniel7328@gmail.com>
2025-03-06 13:23:48 -0500

    feat: :sparkles: Currently working on photo uploader

--- DatingApp/Controllers/UsersController.cs
+++ DatingApp/Controllers/UsersController.cs
@@ -1,17 +1,17 @@
 ﻿using System.Security.Claims;
 using AutoMapper;
-using DatingApp.Data;
 using DatingApp.DTOs;
 using DatingApp.Entities;
+using DatingApp.Extensions;
 using DatingApp.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.Controllers
 {
     [Authorize]
-    public class UsersController(IUserRepository userRepository, IMapper mapper) : BaseApiController
+    public class UsersController(IUserRepository userRepository, 
+        IMapper mapper, IPhotoService photoService) : BaseApiController
     {
 
         [HttpGet]
@@ -37,11 +37,7 @@ namespace DatingApp.Controllers
         [HttpPut]
         public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
         {
-            var name = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
-            
-            if(name == null) return BadRequest("No username found in token");
-
-            var user = await userRepository.GetUserByNameAsync(name);
+            var user = await userRepository.GetUserByNameAsync(User.GetUsername());
 
             if (user == null) return BadRequest("Could not find user");
             
@@ -50,6 +46,30 @@ namespace DatingApp.Controllers
             if(await userRepository.SaveAllAsync()) return NoContent();
             
             return BadRequest("Failed to update user");
+        }
+
+        [HttpPost("add-photo")]
+        public async Task<ActionResult<PhotoDto>> AddPhoto(IFormFile file)
+        {
+            var user = await userRepository.GetUserByNameAsync(User.GetUsername());
+            
+            if (user == null) return BadRequest("Could not find user");
+            
+            var result = await photoService.AddPhotoAsync(file);
+            
+            if(result.Error != null) return BadRequest(result.Error.Message);
+
+            var photo = new Photo
+            {
+                Url = result.SecureUrl.AbsoluteUri,
+                PublicId = result.PublicId,
+            };
+            
+            user.Photos.Add(photo);
+            if(await userRepository.SaveAllAsync()) 
+                return CreatedAtAction(nameof(GetUserByName)
+                    , new {name = user.Name}, mapper.Map<PhotoDto>(photo));
+            return BadRequest("Failed to add photo");
         }
 
     }
--- DatingApp/Extensions/ClaimsPrincipleExtensions.cs
+++ DatingApp/Extensions/ClaimsPrincipleExtensions.cs
@@ -1,6 +1,15 @@
+using System.Security.Claims;
+
 namespace DatingApp.Extensions;
 
-public class ClaimsPrincipleExtensions
+public static class ClaimsPrincipleExtensions
 {
-    
+    public static string GetUsername(this ClaimsPrincipal user)
+    {
+        var username = user.FindFirstValue(ClaimTypes.NameIdentifier);
+        
+        if(username == null) throw new Exception("Cannot get username from token");
+
+        return username;
+    }
 }
